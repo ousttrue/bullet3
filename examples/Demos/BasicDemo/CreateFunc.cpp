8bd43a84971447f0bd03dcb1d7bced189bb1cce7
ousttrue <ousttrue@gmail.com>
2022-01-14 10:54:48 +0900

    BasicDemo

---
+++ examples/Demos/BasicDemo/CreateFunc.cpp
@@ -0,0 +1,5 @@
+// this define maps StandaloneExampleCreateFunc to the right 'CreateFunc'
+#define B3_USE_STANDALONE_EXAMPLE 1
+#include <CommonExampleInterface.h>
+#include "BasicExample.h"
+B3_STANDALONE_EXAMPLE(BasicExampleCreateFunc)
--- examples/ExampleBrowser/ExampleEntries.cpp
+++ examples/ExampleBrowser/ExampleEntries.cpp
@@ -13,7 +13,7 @@
 
 #include <ForkLift/ForkLiftDemo.h>
 #include <MultiThreadedDemo/MultiThreadedDemo.h>
-#include "../BasicDemo/BasicExample.h"
+#include <BasicDemo/BasicExample.h>
 #include <Planar2D/Planar2D.h>
 #include <Benchmarks/BenchmarkDemo.h>
 #include "../Importers/ImportObjDemo/ImportObjExample.h"
