959137e94065c11b84d1d61ac77d372ad232effb
ousttrue <ousttrue@gmail.com>
2022-01-21 16:50:40 +0900

    VAO

--- examples/GLFont/GLMesh.cpp
+++ examples/GLFont/GLMesh.cpp
@@ -9,26 +9,24 @@ GLMesh::GLMesh(const std::shared_ptr<GLVBO> &vbo, const std::shared_ptr<GLIBO> &
 	: m_vbo(vbo), m_ibo(ibo)
 {
 	m_vao = GLVAO::create();
-	m_vao->bind();
-	m_vbo->bind();
-	m_ibo->bind();
+	{
+		// setup vao
+		m_vao->bind();
+		m_vbo->bind();
+		m_ibo->bind();
+		glEnableVertexAttribArray(0);
+		glEnableVertexAttribArray(1);
+		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(PrimVertex), (const GLvoid *)0);
+		glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(PrimVertex), (const GLvoid *)sizeof(PrimVec4));
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(PrimVertex), (const GLvoid *)(sizeof(PrimVec4) + sizeof(PrimVec4)));
+	}
 	m_vao->unbind();
 }
 
 void GLMesh::draw(int indexCount)
 {
 	m_vao->bind();
-	m_vbo->bind();
-	m_ibo->bind();
-	glEnableVertexAttribArray(0);
-	glEnableVertexAttribArray(1);
-	glEnableVertexAttribArray(2);
-	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(PrimVertex), (const GLvoid *)0);
-	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(PrimVertex), (const GLvoid *)sizeof(PrimVec4));
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(PrimVertex), (const GLvoid *)(sizeof(PrimVec4) + sizeof(PrimVec4)));
 	glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, 0);
-	glDisableVertexAttribArray(0);
-	glDisableVertexAttribArray(1);
-	glDisableVertexAttribArray(2);
 	m_vao->unbind();
 }
